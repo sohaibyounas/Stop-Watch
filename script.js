6258bfdff453bea9864c0f25b5ccf63aac44482e
sohaibyounas <sohaibyounas24@gmail.com>
2025-03-19 16:05:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayTime = document.getElementById('displayTime');
+let timer = null;
+
+function stopwatch(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++;
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+// watch start
+function watchStart(){
+    if(timer !== null){
+        clearInterval(timer);
+    }
+    
+    timer = setInterval(stopwatch, 1000);
+}
+
+// watch stop
+function watchStop() {
+    if (timer !== null) {
+        clearInterval(timer);
+        timer = null;
+    }
+}
+
+// watch reset
+function watchReset(){
+    clearInterval(timer);
+
+    [seconds, minutes, hours] = [0, 0, 0];
+    displayTime.innerHTML = "00:00:00";
+}
